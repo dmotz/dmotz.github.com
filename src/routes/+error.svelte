7424c2a80f9baba365e47f861b68da09e384f9aa
Dan Motzenbecker <dan@oxism.com>
2023-07-31 18:29:55 -0400

    error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import '../styles/main.sass'
+</script>
+
+<section class="e404">
+  <h1>
+    {#each '404'.split('') as char, i}
+      <span style:animation-delay="{i * 0.2}s">{char}</span>
+    {/each}
+  </h1>
+  <a href="/">return to /</a>
+</section>
