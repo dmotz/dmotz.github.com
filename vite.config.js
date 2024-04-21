b46b0dfe6d7a1d809a89540d541dbd837d59104f
Dan Motzenbecker <dan@oxism.com>
2024-04-21 12:05:23 -0400

    enhanced image attempt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import {defineConfig} from 'vite'
 import {sveltekit} from '@sveltejs/kit/vite'
-// import {enhancedImages} from '@sveltejs/enhanced-img'
+import {enhancedImages} from '@sveltejs/enhanced-img'
 import md from 'vite-plugin-markdown'
 
 export default defineConfig({
   server: {port: 3333},
-  plugins: [/*enhancedImages(),*/ md.plugin({mode: 'html'}), sveltekit()]
+  plugins: [enhancedImages(), md.plugin({mode: 'html'}), sveltekit()]
 })
